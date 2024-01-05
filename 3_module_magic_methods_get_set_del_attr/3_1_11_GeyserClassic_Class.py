4b6dc34f2d115be95f80df4a518422c02bbb41da
Mikhail Tabakaev <47657224+Mihakgma@users.noreply.github.com>
2024-01-05 12:43:39 +0700

    3_1_11 step has been updated but not solved yet

--- 3_module_magic_methods_get_set_del_attr/3_1_11_GeyserClassic_Class.py
+++ 3_module_magic_methods_get_set_del_attr/3_1_11_GeyserClassic_Class.py
@@ -63,5 +63,87 @@ P.S. На экран ничего выводить не нужно.
 """
 
 
+class Filter:
+    _filter_installed = False
+
+    def __init__(self, date):
+        self.date = date
+
+    def __setattr__(self, key, value):
+        if self._filter_installed:
+            return
+        elif isinstance(value, float) and value >= 0:
+            self._filter_installed = True
+            super().__setattr__(key, value)
+
+
+class Mechanical(Filter):
+    pass
+
+
+class Aragon(Filter):
+    pass
+
+
+class Calcium(Filter):
+    pass
+
+
+class GeyserClassic:
+    MAX_DATE_FILTER = 100
+    filters_codes = {
+        1: Mechanical,
+        2: Aragon,
+        3: Calcium
+    }
+
+    def __init__(self):
+        self.filters_installed = {
+            1: None,
+            2: None,
+            3: None
+        }
+
+    @classmethod
+    def verify_filter(cls, slot_num, filter=False):
+        filters = cls.filters_codes
+        if not filter:
+            return slot_num in filters
+        if slot_num in filters and [True for (k, v) in filters.items() if v == filter]:
+            return True
+
+    def add_filter(self, slot_num, filter):
+        """
+        добавление фильтра filter в указанный слот slot_num (номер слота: 1, 2 и 3),
+        если он (слот) пустой (без фильтра).
+        Также здесь следует проверять, что в первый слот можно установить только объекты класса Mechanical,
+        во второй - объекты класса Aragon и в третий - объекты класса Calcium. Иначе слот должен оставаться пустым.
+        :param slot_num:
+        :param filter:
+        :return:
+        """
+        # повторное добавление в занятый слот - невозможно
+        if self.verify_filter(slot_num, filter) and self.filters_installed[slot_num] is None:
+            self.filters_installed[slot_num] = filter
+
+    def remove_filter(self, slot_num):
+        if self.verify_filter(slot_num):
+            self.filters_installed[slot_num] = None
+
+    def get_filters(self):
+        installed_filters = self.filters_installed
+        return (v for (k, v) in installed_filters.items())
+
+    def water_on(self):
+        """
+        True, если выполнены условия:
+        - все три фильтра установлены в слотах;
+        - все фильтры работают в пределах срока службы
+        (значение (time.time() - date) должно быть в пределах [0; MAX_DATE_FILTER])
+        :return: True | False
+        """
+        pass
+
+
 if __name__ == '__main__':
     pass
