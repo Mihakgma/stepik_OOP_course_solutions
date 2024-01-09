8941ada0ddd1318613c1ece1e56618201aed192b
Mikhail Tabakaev <47657224+Mihakgma@users.noreply.github.com>
2024-01-09 23:13:32 +0700

    3_2_1 lection materials have been updated

--- 3_module_magic_methods_get_set_del_attr/3_2_1_Decorators_Addition_Material_2.py
+++ 3_module_magic_methods_get_set_del_attr/3_2_1_Decorators_Addition_Material_2.py
@@ -2,6 +2,7 @@
 https://www.youtube.com/watch?v=v0qZPplzwUQ&t=611s
 """
 # Декораторы. Способы применения.
+import time
 
 
 def func_decorator(func):
@@ -11,9 +12,13 @@ def func_decorator(func):
     :return: результат выполнения обернутой функции
     """
     def wrapper(*args, **kwargs):
-        print("----------- что-то делаем перед вызовом функции -----------")
+        # print("----------- что-то делаем перед вызовом функции -----------")
+        st = time.time()
         res = func(*args, **kwargs)
-        print("----------- что-то делаем после  вызова функции -----------")
+        et = time.time()
+        # print("----------- что-то делаем после  вызова функции -----------")
+        dt = et - st
+        print(f"Время работы: {dt} сек.")
         return res
 
     return wrapper
@@ -24,8 +29,40 @@ def some_func(message):
     return True
 
 
+@func_decorator
+def get_nod(a, b):
+    """
+    Медленный алгоритм Евклида
+    :param a:
+    :param b:
+    :return:
+    """
+    while a != b:
+        if a > b:
+            a -= b
+        else:
+            b -= a
+    return a
+
+
+@func_decorator
+def get_fast_nod(a, b):
+    if a < b:
+        a, b = b, a
+    while b:
+        a, b = b, a % b
+    return a
+
+
 if __name__ == '__main__':
     # пример "обертывания" функции в декоратор
     f = func_decorator(func=some_func)
     r = f("Python ForEver!!!")
     print(r)
+
+    # get_nod = func_decorator(get_nod)
+    # get_fast_nod = func_decorator(get_fast_nod)
+
+    res = get_nod(2, 1000000)
+    res2 = get_fast_nod(2, 1000000)
+    print(res, res2)
