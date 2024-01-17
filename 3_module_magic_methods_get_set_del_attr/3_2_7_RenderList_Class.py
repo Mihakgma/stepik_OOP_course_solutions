b6f99f37ced006620cff010843fd7fbe6a9b0e5e
Mikhail Tabakaev <47657224+Mihakgma@users.noreply.github.com>
2024-01-17 19:06:27 +0700

    3_2_7 and 3_2_8 steps have been solved

--- 3_module_magic_methods_get_set_del_attr/3_2_7_RenderList_Class.py
+++ 3_module_magic_methods_get_set_del_attr/3_2_7_RenderList_Class.py
@@ -30,6 +30,39 @@ P.S. На экран ничего выводить не нужно.
 """
 
 
+class RenderList:
+    tags_approved = {
+        "ol": ["<ol>", "</ol>"],
+        "ul": ["<ul>", "</ul>"]
+    }
+    default_tag = "ul"
+
+    def __init__(self, type_list: str):
+        """
+        type_list - тип списка
+        (принимает значения: "ul" - для списка с тегом <ul> и "ol" - для списка с тегом <ol>).
+        Если значение параметра type_list другое (не "ul" и не "ol"), то формируется список с тегом <ul>.
+        """
+        self.type_list = type_list if type_list in self.tags_approved else self.default_tag
+
+    def get_tags(self, tags_type):
+        tags_info = self.tags_approved
+        if isinstance(tags_type, str):
+            return [tags_info[self.default_tag], tags_info[tags_type]][tags_type in tags_info]
+        return
+
+    def __call__(self, *args, **kwargs):
+        statement_rows = args[0]
+        txt = '\n'.join([f'<li>{row}</li>' for row in statement_rows])
+        tags = self.tags_approved[self.type_list]
+        return f'{tags[0]}\n{txt}\n{tags[1]}'
+
+
 # Далее - для проверки:
 if __name__ == '__main__':
-    pass
+    tg_1 = RenderList("sdfnsdfsdf")
+    print(tg_1.__dict__)
+    lst = ["Пункт меню 1", "Пункт меню 2", "Пункт меню 3"]
+    render = RenderList("ol")
+    html = render(lst)
+    print(html)
